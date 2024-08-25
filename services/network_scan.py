b9ffb506764a77a94ab304dadbbd7d04af58fe66
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2024-08-25 19:16:02 +0200

    Fix: logic on ip scan function

--- services/network_scan.py
+++ services/network_scan.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+from re import findall
 
 class Scan:
     def __init__(self, base_ip):
@@ -9,37 +10,56 @@ class Scan:
     def ping_ip(ip):
         print(f'Scanning IP: {ip}', end=' ... ')
 
-
         param = "-n" if platform.system().lower() == "windows" else "-c"
-        command = ["ping", param, "1", ip]
+        ttl_data = ""
+        output = False
 
-        response = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        if response.returncode == 0:
+        command = ["ping", param, "1", ip]
+        response = subprocess.Popen(command, stdout=subprocess.PIPE)
+        response.wait()
+
+
+        for line in response.stdout:
+            ttl_data = ttl_data + str(line)
+            find_ttl = findall("TTL", ttl_data)
+
+
+        if find_ttl:
             status = "Ativo"
             status_char = "✔"
+            output = True
         else:
             status = "Inativo"
             status_char = "✘"
         
         print(f"{status_char} {status}")
         
-        return response.returncode == 0
+        return output
 
     def scan_network(self):
         
         active_ips = []
-
-        # split the prefix of 3 octets and ignore the last octet of IP
         prefix = '.'.join(self.base_ip.split('.')[:2])
-
-        for i in range(0, 256):
-            for j in range(1, 256):
-                ip = f"{prefix}.{i}.{j}"
-                if self.ping_ip(ip):
-                    print(f"Active IP Found: {ip}")
-                    active_ips.append(ip)
+        test_last_2 = True
         
+        if int(self.base_ip.split('.')[2]) != 0 and int(self.base_ip.split('.')[3]) == 0:
+            prefix = '.'.join(self.base_ip.split('.')[:3])
+            test_last_2 = False
+
+
+        if test_last_2:
+            for i in range(0, 256):
+                for j in range(1, 256):
+                    ip = f"{prefix}.{i}.{j}"
+                    if self.ping_ip(ip):
+                        active_ips.append(ip)
+        else:
+            for i in range(0,256):
+                ip = f"{prefix}.{i}"
+                if self.ping_ip(ip):
+                    active_ips.append(ip)
+
+
         # Save Ips in a txt
         if active_ips:
             with open("active_ip_list.txt", "w") as f:
