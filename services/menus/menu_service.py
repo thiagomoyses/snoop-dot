c8d47ec8ee3cf846daf29bc7ddf5e390a52bac45
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2024-09-12 16:26:19 +0200

    update menus

--- services/menus/menu_service.py
+++ services/menus/menu_service.py
@@ -1,49 +1,33 @@
-import argparse
-from services import Scan
+from .submenus.sub_menus_01 import SubMenu01
 from colorama import Fore, Style
 from pyfiglet import figlet_format
-import sys
 
 class Menu:
 
-    @staticmethod
-    def main_menu():
+    def __init__(self):
+        self.main_options = [
+            "IP-SNIFFER",
+            "DIRECTORY BRUTE-FORCE"
+        ]
+
+    def main_menu(self):
         
         # Welcome
         title = figlet_format("SNOOP-DOT", font="starwars", width=200)
         print(Fore.GREEN + title + Style.RESET_ALL)
         print(Fore.BLUE + "Created by:" + Fore.RED + " Thiago da Silva Moyses" + Style.RESET_ALL)
-        print(Fore.BLUE + "GitHub: " + Fore.GREEN + "https://github.com/thiagomoyses" + Style.RESET_ALL)
-        print()
+        print(Fore.BLUE + "GitHub: " + Fore.GREEN + "https://github.com/thiagomoyses\n\n\n" + Style.RESET_ALL)
 
-        output_file = False
-        netbox_output = False
-        
-        parser = argparse.ArgumentParser(description="Scan network looking for active IPs")
-        parser.add_argument("base_ip", help="The ip base to be scanned, with the following format x.x.0.0 (ex: 192.168.0.0)")
-        parser.add_argument("--output", help="Output the scan results to a file")
-        parser.add_argument("--netbox", help="Output the scan results to Netbox")
-        args = parser.parse_args()
+        # Choices
+        while True:
+            print(Fore.YELLOW + Style.BRIGHT + "=== MAIN MENU ===\n" + Style.RESET_ALL)
+            for index, option in enumerate(self.main_options, start=1):
+                print(Fore.BLUE + f"{index}. {option}" + Style.RESET_ALL)
+            print(Fore.YELLOW + "================\n" + Style.RESET_ALL)
 
-        if args.output:
-            if args.output == 'true':
-                output_file = True
-            elif args.output == 'false':
-                output_file = False
-            else:
-                print(Fore.RED + "Invalid value for --output. Accepted values are 'true' or 'false'." + Style.RESET_ALL)
-                parser.print_help()
-                sys.exit(1)
-
-        if args.netbox:
-            if args.netbox == 'true':
-                netbox_output = True
-            elif args.netbox == 'false':
-                netbox_output = False
-            else:
-                print(Fore.RED + "Invalid value for --netbox. Accepted values are 'true' or 'false'." + Style.RESET_ALL)
-                parser.print_help()
-                sys.exit(1) 
-
-        scanner = Scan(args.base_ip, output_file, netbox_output)
-        scanner.scan_network()
+            try:
+                choice = int(input(Fore.CYAN + "Please choose an option: "))
+                runner = SubMenu01()
+                runner.handle_option(choice)
+            except ValueError:
+                print(Fore.RED + "Invalid input. Please enter a number.")
--- services/menus/submenus/sub_menus_01.py
+++ services/menus/submenus/sub_menus_01.py
@@ -14,7 +14,7 @@ class SubMenu01:
     def brute_force_menu(self):
 
         while True:
-            print(Fore.YELLOW + Style.BRIGHT + "=== OPTIONS ===\n" + Style.RESET_ALL)
+            print(Fore.YELLOW + Style.BRIGHT + "=== DIR OPTIONS ===\n" + Style.RESET_ALL)
 
             for index, option in enumerate(self.options["01"], start=1):
                 print(Fore.BLUE + f"{index}. {option}" + Style.RESET_ALL)
@@ -22,11 +22,13 @@ class SubMenu01:
 
             try:
                 choice = int(input(Fore.CYAN + "Please choose an option: "))
-                print('here')
             except ValueError:
                 print(Fore.RED + "Invalid input. Please enter a number.")
 
-
-if __name__ == '__main__':
-    runner = SubMenu01()
-    runner.brute_force_menu()
+    def handle_option(self, option):
+        match option:
+            case 2:
+                self.brute_force_menu()
+            
+            case _:
+                print('Invalid Option')
